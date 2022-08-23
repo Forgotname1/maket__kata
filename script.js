778a98fbf3edb83cdfde431fc04178a740ab79f5
Forgotname1 <107539219+Forgotname1@users.noreply.github.com>
2022-08-23 13:30:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,88 +1,52 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    },
-    grabCursor:true,
-    slidesPerView: 1.3,
-    spaceBetween:5,
-   /* spaceBetween: 5,
-    freeMode:true*/
-});
-let readMore = document.querySelector('.readMore');
-let readLess = document.querySelector('.readLess');
-let mainBlock = document.querySelector('.main');
+(function() {
 
-readMore.addEventListener('click', function (){
-    mainBlock.classList.add('showAll');
-    readMore.classList.add('none');
-    readLess.classList.remove('none');
-})
-readLess.addEventListener('click', function (){
-    mainBlock.classList.remove('showAll');
-    readMore.classList.remove('none');
-    readLess.classList.add('none');
-})
-/*
-function readMore(){
-    let more = document.getElementById('readMore');
-    let btn = document.getElementById('btn');
+    'use strict';
 
-    if(more.style.display === 'none'){
-        btn.innerHTML = 'скрыть';
-        more.style.display = 'flex';
+    const breakpoint = window.matchMedia( '(min-width: 325px)' );
+    let mySwiper;
+    const breakpointChecker = function() {
+
+        if ( breakpoint.matches === true ) {
+            if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
+            return;
+
+        } else if ( breakpoint.matches === false ) {
+            return enableSwiper();
+        }
+    };
+
+    const enableSwiper = function () {
+    mySwiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true,
+        },
+        mousewheel: {
+            sensitivity: 0.5,
+            evntsTarget: '.swiper',
+        },
+        grabCursor:true,
+        slidesPerView: 1.3,
+        freemode: true,
+    });
+};
+    breakpoint.addListener(breakpointChecker);
+    breakpointChecker();
+})();
+
+let main = document.querySelector('.swiper');
+let button = document.querySelector('.readMore');
+let icon =document.querySelector('.icon');
+button.addEventListener('click', function (){
+    main.classList.toggle('showAll');
+    button.classList.toggle('showLess');
+    if (!button.classList.toggle('showAll')) {
+        button.textContent = 'Показать все';
+        icon.classList.remove('rotate');
     }else {
-        btn.innerHTML = 'показать все';
-        more.style.display = 'none';
-
-    }
-
-}
-
-
-*/
-
-/*let btnReadMore = document.querySelector('.btn-readmore');
-let secondBlock = document.querySelector('.second-row-blocks');
-let hiddenBlocks = secondBlock.querySelectorAll('.hidden');
-
-
-
-btnReadMore.addEventListener('click', function (evt){
-    evt.preventDefault();
-    if(hiddenBlocks.style.display === 'none'){
-        hiddenBlocks.style.display = 'block';
-        btnReadMore.innerHTML = '12345'
-    }
-})
-*/
-
-/*let btnReadMore = document.querySelector(".btn-readmore");
-let hiddenBlocks = document.querySelectorAll(".hidden");
-let allBlocks = document.querySelector(".main").children;
-
-btnReadMore.addEventListener('click', function (evt)
-{
-    evt.preventDefault();
-    let itemsCount2Hide = window.screenX > 320 && window.screenX < 1120 ? 5 : 3;
-
-    if (hiddenBlocks.length)
-    {
-        for (const element of hiddenBlocks)
-        {
-            element.classList.remove("hidden");
-        }
-        btnReadMore.textContent='Скрыть';
-    }
-    else
-    {
-        for (let index = allBlocks.length; index > allBlocks.length-itemsCount2Hide; index--)
-        {
-            allBlocks[index].classList.add("hidden");
-        }
-        btnReadMore.textContent='Показать';
+        button.textContent = 'Скрыть';
+        icon.classList.add('rotate');
     }
 });
-*/
